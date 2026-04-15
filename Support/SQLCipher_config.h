67db3aa5a631d7777a5a7d8e066c2d3b0fbc09b7
Jayce Miskel <jayce@miskelpsychiatry.com>
2026-04-15 11:37:23 -0500

    fix(fork): re-export SQLCipher as CSQLite for SPM resolution

--- Support/SQLCipher_config.h
+++ Support/SQLCipher_config.h
@@ -29,4 +29,25 @@ static inline void _enableDoubleQuotedStringLiterals(sqlite3 *db) {
 static inline void _disableDoubleQuotedStringLiterals(sqlite3 *db) { }
 static inline void _enableDoubleQuotedStringLiterals(sqlite3 *db) { }
 #endif
+
+// Expose APIs that are missing from bundled <sqlite3.h> variants.
+#ifdef GRDB_SQLITE_ENABLE_PREUPDATE_HOOK
+SQLITE_API void *sqlite3_preupdate_hook(
+  sqlite3 *db,
+  void(*xPreUpdate)(
+    void *pCtx,                   /* Copy of third arg to preupdate_hook() */
+    sqlite3 *db,                  /* Database handle */
+    int op,                       /* SQLITE_UPDATE, DELETE or INSERT */
+    char const *zDb,              /* Database name */
+    char const *zName,            /* Table name */
+    sqlite3_int64 iKey1,          /* Rowid of row about to be deleted/updated */
+    sqlite3_int64 iKey2           /* New rowid value (for a rowid UPDATE) */
+  ),
+  void*
+);
+SQLITE_API int sqlite3_preupdate_old(sqlite3 *, int, sqlite3_value **);
+SQLITE_API int sqlite3_preupdate_count(sqlite3 *);
+SQLITE_API int sqlite3_preupdate_depth(sqlite3 *);
+SQLITE_API int sqlite3_preupdate_new(sqlite3 *, int, sqlite3_value **);
+#endif /* GRDB_SQLITE_ENABLE_PREUPDATE_HOOK */
 #endif /* grdb_config_h */
